be16a056cebaa54359bbe36a1a08c47ad3a72926
Albert Oh <albertopha@hotmail.com>
2018-05-01 22:14:11 -0400

    Spring boot set up

---
+++ src/main/java/com/usedTrade/usedTrade/UsedTradeApplication.java
@@ -0,0 +1,12 @@
+package com.usedTrade.usedTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradeApplication.class, args);
+	}
+}
