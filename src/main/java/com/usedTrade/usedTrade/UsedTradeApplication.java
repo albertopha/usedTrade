d15c27c293b65286431b06ba29b460b24c9631e4
LPUS015\Albert Oh <albert.oh@sulzer-us.com>
2018-05-04 11:27:51 -0400

    properties file

--- src/main/java/com/usedTrade/usedTrade/UsedTradeApplication.java
+++ src/main/java/com/usedTrade/usedTrade/UsedTradeApplication.java
@@ -1,14 +1,8 @@
 package com.usedTrade.usedTrade;
 
-import com.usedTrade.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import java.util.Set;
 
 @SpringBootApplication
 public class UsedTradeApplication {
